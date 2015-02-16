df751bfe128b6077278d89dc7fe1def5ce6ac25d
igoryen <igor.yentaltsev@gmail.com>
2015-02-15 19:55:55 -0500

    step6 - Create a Layout

--- quickstart/application/Bootstrap.php
+++ quickstart/application/Bootstrap.php
@@ -1,7 +1,12 @@
 <?php
 // 1
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap{
-
+  // 4
+  protected function _initDoctype(){
+    $this->bootstrap('view'); // 6
+    $view = $this->getResource('view'); // 7
+    $view->doctype('XHTML1_STRICT'); // 8
+  }
 
 }
 
